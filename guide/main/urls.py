363836b51bd377f01bacddaa7abb709d618a2c3b
Cait Powell <powellcm@cardiff.ac.uk>
2021-04-20 18:47:49 +0100

    Add url routes for new pages, mess with colouring

--- guide/main/urls.py
+++ guide/main/urls.py
@@ -4,7 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='main'),
     path('age-check', views.HomePageView.as_view(template_name="age-check.html")),
-    path('your-rights', views.HomePageView.as_view(template_name="your-rights.html")),
     path("5-8", views.YoungerChildPageView.as_view(), name='5-8'),
+    path('your-rights', views.HomePageView.as_view(template_name="your-rights.html")),
+    path("your-education", views.HomePageView.as_view(), name='your-education'),
+    path("your-wellbeing", views.HomePageView.as_view(), name='your-wellbeing'),
+    path("your-voice", views.HomePageView.as_view(), name='your-voice'),
+    path("your-skills", views.HomePageView.as_view(), name='your-skills'),
   #  path("16+", views.HomePageView.as_view(template_name="16+.html"))
 ]
