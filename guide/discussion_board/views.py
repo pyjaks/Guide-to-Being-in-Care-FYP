e849dc3b2a795956d0202777c7b63dd4a2e2b02c
Cait Powell <powellcm@cardiff.ac.uk>
2021-04-27 21:52:13 +0100

    add view for posts view by category

--- guide/discussion_board/views.py
+++ guide/discussion_board/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.views.generic import DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import DetailView, ListView
 from .models import *
 from .forms import *
 from main.views import HomePageView
@@ -19,4 +19,13 @@ class DiscussionBoardDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class DiscussionBoardPostsView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, title=self.kwargs['slug'])
+        return Post.objects.filter(categories=self.category)
+
 
