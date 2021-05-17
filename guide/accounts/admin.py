e1f70a98f36c805194d21d192325ba32bee8e8f0
Cait Powell <powellcm@cardiff.ac.uk>
2021-05-18 00:55:34 +0100

    Fix admin form layout

--- guide/accounts/admin.py
+++ guide/accounts/admin.py
@@ -9,16 +9,16 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'date_of_birth']
+    list_display = ['username', 'email', 'date_of_birth']
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('username', 'email', 'date_of_birth', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'is_staff', 'is_active')}
+            'fields': ('username', 'email', 'date_of_birth', 'password1', 'password2',  'is_staff', 'is_active')}
          ),
     )
 
