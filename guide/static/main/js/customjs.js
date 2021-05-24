1c4bc757dc9a14067ed0bcc805085f4bb109a7f4
Cait Powell <powellcm@cardiff.ac.uk>
2021-05-24 17:06:10 +0100

    Add javascript to only show reply/comment fields on button click

---
+++ guide/static/main/js/customjs.js
@@ -0,0 +1,12 @@
+
+function showComment() {
+    var commentArea = document.getElementById("comment-area");
+        commentArea.classList.remove("hide");
+}
+
+
+function showReply(id){
+    var replyArea = document.getElementById(id);
+    replyArea.classList.remove("hide");
+}
+
